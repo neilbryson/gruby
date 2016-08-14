01589c9732ead14e23ba010d85eb0b80a78db291
Neil Bryson Cargamento <neilbryson@live.com>
2016-08-14 21:11:25 +0800

    initial commit for google drive

---
+++ src/gdrive.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+require_relative 'googleAuth.rb'
+
+module NeilBryson
+  module GoogleApis
+
+    class GDrive
+
+      @@googleAuth = NeilBryson::GoogleAuth
+
+      @@gdrive = Google::Apis::DriveV2
+
+      @@service = @@gdrive::DriveService.new
+
+      @@scope = @@gdrive::AUTH_DRIVE_READONLY
+
+      @@defaultCredentialsFilePath = File.join(File.dirname(__FILE__), 
+        '../conf/credentials.json')
+
+      def initialize(credentialsJsonPath = @@defaultCredentialsFilePath, 
+        scope = @@scope, service = @@service)
+        @credentialsJsonPath = credentialsJsonPath
+        @scope = scope
+        @service = service
+        # Authorise the app and retrieve the access token
+        @ccessToken = @@googleAuth.new(@scope, @credentialsJsonPath, @service)
+          .authorise
+      end
+
+      def listFiles
+        files = @service.list_files
+        fileList = files.items
+      end
+
+    end
+
+  end
+
+end
